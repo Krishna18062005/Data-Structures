deaec2a3703c628307a9e80c881693f23cc0e43b
Krishna K <krish1826s@gmail.com>
2024-07-05 10:31:40 +0530

    Update springboot.java

--- springboot.java
+++ springboot.java
@@ -1,32 +1,34 @@
-// spring is a java framework
-// we take an opinionated view of spring platform,if you want it can be changed
-// we can integrated any framework
-// jar java archive file zipped file
-// war file contains web related servers it provides database too
-// jsp java server pages
-// spring features:
-// pojo - its a class file its a encapsulated file plain old object,dependency
-// object,dependency injection,rest Api
-// class file private variables ,getter and setter methods
-// mvc --> model view controller model--> data structure view--> defines
-// display,controller --> control logic
-// spring allows dynamic languages
-// group id package name
-// artifact if file name
-// jar for java project -->version if web use war;
-// install steps
-// extract contents
-// open sts422 release...;
+/*spring is a java framework
+we take an opinionated view of spring platform,if you want it can be changed
+we can integrated any framework
+jar java archive file zipped file
+war file contains web related servers it provides database too
+jsp java server pages
+spring features:
+pojo - its a class file its a encapsulated file plain old object,dependency
+object,dependency injection,rest Api
+class file private variables ,getter and setter methods
+mvc --> model view controller model--> data structure view--> defines
+display,controller --> control logic
+mvc allows only java
+spring framework allows dynamic languages
+group id package name
+artifact if file name
 
-// public class springboot{
-// private int a;
-// private int b;
-// public void setter(){
-// a=8;
-// b=9;
-// }
-// public void getter(){
-// System.out.println(a+" "+b);
-// }
+jar for java project -->version if web use war;
+install steps
+extract contents
+open sts422 release...;
 
-// }
+public class springboot{
+private int a;
+private int b;
+public void setter(){
+a=8;
+b=9;
+}
+public void getter(){
+System.out.println(a+" "+b);
+}
+
+}*/
