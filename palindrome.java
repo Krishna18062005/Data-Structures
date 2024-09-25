c10b921f858a0d67d06987ecee128c33388caed0
Krishna K <krish1826s@gmail.com>
2024-09-25 09:18:24 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Driver_code {
+    static Node insert(Node head, int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+            return head;
+        } else {
+            Node t = head;
+            while (t.next != null) {
+                t = t.next;
+            }
+            t.next = temp;
+        }
+        return head;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            Node head = null;
+
+            String str[] = read.readLine().trim().split(" ");
+            int listSize = str.length;
+            for (int i = 0; i < listSize; i++) {
+                head = insert(head, Integer.parseInt(str[i]));
+            }
+            boolean f = new Solution().isPalindrome(head);
+
+            System.out.println(f ? "true" : "false");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of class Node is
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether the list is palindrome.
+    boolean isPalindrome(Node head) {
+        // Your code here
+        Node copy=new Node(head.data);
+        Node c1=head;
+        
+        Node curr=head,prev=null,next;
+        while(curr!=null){
+            next=curr.next;
+            copy.next=new Node(head.data);
+            copy=copy.next;
+            prev=curr;
+            curr=next;
+        }
+        
+        Node c2=prev;
+        while(c2!=null){
+            if(c1.data!=c2.data) return false;
+            
+            c2=c2.next;
+            c1=c1.next;
+        }
+        return true;
+    }
+}
