32ccd426d43f4825aee35e17ba22fe06c900a825
krishna18062005 <krish1826s@gmail.com>
2025-04-25 22:09:32 +0530

    Code

---
+++ BinToLL.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        
+        String ans="";
+        while(head!=null){
+            ans+=head.val;
+            head=head.next;
+        }
+        return Integer.parseInt(ans,2);
+    }
+}
